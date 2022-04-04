61b8cdd58b5e9651915d4f709493feedaa87014e
Mauricio Silva <mauriciospta@gmail.com>
2022-04-03 22:05:19 -0300

    implementa a funcao de bloquear as rotas da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "./components/Layout";
-import { ArtigoPage } from "./pages/Artigo";
-import { ArtigosPage } from "./pages/Artigos";
-import { EditarArquivoPage } from "./pages/EditarArquivo";
-import { LoginPage } from "./pages/Login";
-import { MeusArtigosPage } from "./pages/MeusArtigos";
-import { NotFoundPage } from "./pages/NotFound";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { RequireAuth } from './components/RequireAuth';
+import { ArtigoPage } from './pages/Artigo';
+import { ArtigosPage } from './pages/Artigos';
+import { EditarArquivoPage } from './pages/EditarArquivo';
+import { LoginPage } from './pages/Login';
+import { MeusArtigosPage } from './pages/MeusArtigos';
+import { NotFoundPage } from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path='/login' element={<LoginPage />} />
 
-        <Route path="/" element={<Layout />}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<ArtigosPage />} />
-          <Route path="/artigo/:id" element={<ArtigoPage />} />
-          <Route path="/artigos" element={<MeusArtigosPage />} />
-          <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
-          <Route path="/artigos/novo" element={<EditarArquivoPage />} />
+          <Route path='/artigo/:id' element={<ArtigoPage />} />
+
+          <Route element={<RequireAuth />}>
+            <Route path='/artigos' element={<MeusArtigosPage />} />
+            <Route path='/artigos/editar/:id' element={<EditarArquivoPage />} />
+            <Route path='/artigos/novo' element={<EditarArquivoPage />} />
+          </Route>
         </Route>
 
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,39 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ActivableLink } from '../ActivableLink';
 
 export const Navigation = () => {
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setAuthenticated(localStorage.getItem('access_token') !== null);
+  }, []);
+
+  function logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id');
+    setAuthenticated(false);
+    navigate('/');
+  }
+
+  if (!isAuthenticated) {
+    return (
+      <>
+        <ActivableLink to='/'>Home</ActivableLink>
+        <ActivableLink to='/login'>Login</ActivableLink>
+      </>
+    );
+  }
   return (
     <>
-      <Link to="/">Home</Link>
-      <Link to="/artigos">Meus Artigos</Link>
-      <Link to="/artigos/novo">Novo Artigo</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/">Logout</Link>
+      <ActivableLink to='/'>Home</ActivableLink>
+      <ActivableLink to='/artigos'>Meus Artigos</ActivableLink>
+      <ActivableLink to='/artigos/novo'>Novo Artigo</ActivableLink>
+      <ActivableLink to='/login'>Login</ActivableLink>
+      <ActivableLink to='/' onClick={logout} type={'button'}>
+        Logout
+      </ActivableLink>
     </>
   );
 };
