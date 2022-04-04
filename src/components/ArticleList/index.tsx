081c924ecd1e747c0d3ef865e1f537a0db3da0f0
Mauricio Silva <mauriciospta@gmail.com>
2022-04-04 14:12:08 -0300

    implementa a exibicao do artigo ao ser clicado

--- src/components/ArticleList/index.tsx
+++ src/components/ArticleList/index.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { ArticleThumbnail } from "../ArticleThumbnail";
-import { ArticleListProps } from "./ArticleList.types";
+import React from 'react';
+import { ArticleThumbnail } from '../ArticleThumbnail';
+import { ArticleListProps } from './ArticleList.types';
 
 export const ArticleList: React.FC<ArticleListProps> = ({ articles }) => {
   return (
-    <div className="flex flex-col items-center justify-items-center m-10">
+    <div className='flex flex-col items-center justify-items-center m-10'>
       {articles.map((article) => (
-        <ArticleThumbnail key={article.titulo} {...article} />
+        <ArticleThumbnail key={article.id} {...article} />
       ))}
     </div>
   );
--- src/pages/Artigo/index.tsx
+++ src/pages/Artigo/index.tsx
@@ -1,33 +1,36 @@
-import faker from "@faker-js/faker";
-import { useEffect, useState } from "react";
-import { ArticleView } from "../../components/ArticleView";
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { ArticleThumbnailProps } from '../../components/ArticleThumbnail/ArticleThumbnail.types';
+import { ArticleView } from '../../components/ArticleView';
+import apiClient from '../../services/api-client';
 
 export const ArtigoPage = () => {
-  const [article, setArticle] = useState<string>("");
-  const [autor] = useState({
-    nome: faker.name.firstName(),
-    avatar: faker.image.avatar(),
-  });
-  const [dataPublicacao] = useState(new Date());
+  const [article, setArticle] = useState<ArticleThumbnailProps>();
+  const { id } = useParams();
 
   useEffect(() => {
     async function loadArticle() {
-      const response = await fetch("/article.md");
-      const article = await response.text();
-      setArticle(article);
+      const url = `/artigos/${id}`;
+      const response = await apiClient.get(url);
+      setArticle({
+        ...response.data,
+        dataPublicacao: new Date(response.data.dataPublicacao),
+      });
     }
 
     loadArticle();
-  }, []);
+  }, [id]);
 
   return (
-    <div>
-      <ArticleView
-        article={article}
-        autor={autor}
-        dataPublicacao={dataPublicacao}
-        tempoLeitura={"10min"}
-      />
+    <div className='ml-60 mr-20 my-10'>
+      {article && (
+        <ArticleView
+          article={article.conteudo}
+          autor={article.autor}
+          dataPublicacao={article.dataPublicacao}
+          tempoLeitura={article.tempoLeitura}
+        />
+      )}
     </div>
   );
 };
